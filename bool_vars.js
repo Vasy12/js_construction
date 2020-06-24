3ca98f948cf59f82774384fc6f15fe706e6da5e8
Oksana Kozlova <lollybird87@gmail.com>
2020-06-24 22:45:29 +0300

    Update file
    Work with construction && ||

--- bool_vars.js
+++ bool_vars.js
@@ -11,8 +11,9 @@ function varIsTrueLongVersion(test) {
         return 'Неверно';
     }
 }
-console.log('varIsTrueLongVersion( true)=',varIsTrueLongVersion(true));
-console.log('varIsTrueLongVersion( false)=',varIsTrueLongVersion(false));
+
+console.log('varIsTrueLongVersion( true)=', varIsTrueLongVersion(true));
+console.log('varIsTrueLongVersion( false)=', varIsTrueLongVersion(false));
 
 /**
  *
@@ -22,8 +23,9 @@ console.log('varIsTrueLongVersion( false)=',varIsTrueLongVersion(false));
 function varIsTrueShortVersion(test) {
     return test ? 'Верно' : 'Неверно';
 }
-console.log('varIsTrueShortVersion( true)=',varIsTrueShortVersion(true));
-console.log('varIsTrueShortVersion( false)=',varIsTrueShortVersion(false));
+
+console.log('varIsTrueShortVersion( true)=', varIsTrueShortVersion(true));
+console.log('varIsTrueShortVersion( false)=', varIsTrueShortVersion(false));
 
 /**
  *
@@ -38,8 +40,9 @@ function varIsNotTrueLongVersion(test) {
     }
 }
 
-console.log('varIsNotTrueLongVersion( true)=',varIsNotTrueLongVersion(true));
-console.log('varIsNotTrueLongVersion( false)=',varIsNotTrueLongVersion(false));
+console.log('varIsNotTrueLongVersion( true)=', varIsNotTrueLongVersion(true));
+console.log('varIsNotTrueLongVersion( false)=', varIsNotTrueLongVersion(false));
+
 /**
  *
  * @param {boolean} test
@@ -48,5 +51,80 @@ console.log('varIsNotTrueLongVersion( false)=',varIsNotTrueLongVersion(false));
 function varIsNotTrueShortVersion(test) {
     return !test ? 'Верно' : 'Неверно';
 }
-console.log('varIsNotTrueShortVersion( true)=',varIsNotTrueShortVersion(true));
-console.log('varIsNotTrueShortVersion( false)=',varIsNotTrueShortVersion(false));
+
+console.log('varIsNotTrueShortVersion( true)=', varIsNotTrueShortVersion(true));
+console.log('varIsNotTrueShortVersion( false)=', varIsNotTrueShortVersion(false));
+
+/**
+ *
+ * @param {number} a
+ * @returns {string}
+ */
+function variable1(a) {
+    if (a > 0 && a < 5) {
+        return 'Верно';
+    } else {
+        return 'Неверно';
+    }
+}
+
+console.log('variable1( 5 )=', variable1(5));
+console.log('variable1( 0 )=', variable1(0));
+console.log('variable1( -3 )=', variable1(-3));
+console.log('variable1( 2 )=', variable1(2));
+
+/**
+ *
+ * @param {number} a
+ * @returns {number}
+ */
+function variable2(a) {
+    if (a === 0 || a === 2) {
+        a = a + 7;
+    } else {
+        a = a / 10;
+    }
+    return a;
+}
+
+console.log('variable2( 5 )=', variable2(5));
+console.log('variable2( 0 )=', variable2(0));
+console.log('variable2( -3 )=', variable2(-3));
+console.log('variable2( 2 )=', variable2(2));
+
+/**
+ *
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+function variable3(a, b) {
+    if (a <= 1 && b >= 3) {
+       return  a + b;
+    } else {
+        return  a - b;
+    }
+}
+
+console.log('variable3( 1 , 3 )=', variable3(1,3));
+console.log('variable3( 0 , 6 )=', variable3(0, 6));
+console.log('variable3( 3, 5)=', variable3(3, 5));
+
+
+/**
+ *
+ * @param {number} a
+ * @param {number} b
+ * @returns {string}
+ */
+function variable4(a, b) {
+    if ((a > 2 && a < 11) || (b >= 6 && b < 14)) {
+        return 'Верно';
+    } else {
+        return 'Неверно';
+    }
+}
+
+console.log('variable4( 3 , 7 )=', variable4(3,7));
+console.log('variable4( 0 , 6 )=', variable4(0, 6));
+console.log('variable4( 10, 6)=', variable4(10, 6));
